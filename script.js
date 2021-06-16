ee6ad133559b4922ff7ad12a98a45e224cfe18c6
bowtiedarcticwolf <bowtiedarcticwolf@gmail.com>
2021-06-15 17:13:39 -0700

    Added web3.js, integration with metamask

--- script.js
+++ script.js
@@ -1,44 +1,83 @@
-const endpointUrl = "https://degenistan.com/";
+const endpointUrl = '/'
 
-function getAccount() {
-  const accountId = $("#accountIDInput").val();
+function getAccount () {
+  const accountId = $('#accountIDInput').val()
 
-  getAccountInfoAjax(accountId).done( (data) => {
-    data = data[0].meta;
-    $("#name").text(data.name);
-    $("#description").text(data.description);
-    $("#imgNft").attr("src", data.image);
-    $("#imgNft").removeClass("visually-hidden");
-  });
+  getAccountInfoAjax(accountId).done((data) => {
+    data = data[0].meta
+    $('#name').text(data.name)
+    $('#description').text(data.description)
+    $('#imgNft').attr('src', data.image)
+    $('#imgNft').removeClass('visually-hidden')
+  })
 }
 
-function getAccountInfoAjax(address) {
-  const endpoint = `${endpointUrl}account`;
+function getAccountInfoAjax (address) {
+  const endpoint = `${endpointUrl}account`
 
-  const data = `{"account":"${address}"}`;
+  const data = `{"account":"${address}"}`
 
   return $.post({
     url: endpoint,
-    contentType: "application/json",
-    accepts: "application/json",
+    contentType: 'application/json',
+    accepts: 'application/json',
     data: data
-  });
+  })
 }
 
-function getBadges() {
-  const accountId = $("#accountIDInput").val();
+function getBadges () {
+  const accountId = $('#accountIDInput').val()
 
-  getBadgesInfoAjax(accountId).done( (data) => {
+  getBadgesInfoAjax(accountId).done((data) => {
     // temp vars
-    const badgeArray = data.d;
-  });
+    const badgeArray = data.d
+  })
 }
 
-function getBadgesInfoAjax(address) {
-  const endpoint = `${endpointUrl}badges?address=${address}`;
+function getBadgesInfoAjax (address) {
+  const endpoint = `${endpointUrl}badges?address=${address}`
 
-  return $.ajax(endpoint,{
-    type: "POST",
-    accepts: "application/json"
-  });
+  return $.ajax(endpoint, {
+    type: 'POST',
+    accepts: 'application/json'
+  })
 }
+
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function () {
+    return await App.initWeb3()
+  },
+
+  initWeb3: async function () {
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum
+      try {
+        await window.ethereum.enable()
+      } catch (error) {
+        // User denied account access...
+        console.error('User denied account access')
+      }
+    } else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
+    }
+    web3 = new Web3(App.web3Provider)
+
+    return App.initContract()
+  },
+  initContract: async function () {
+    web3.eth.getAccounts((error, accounts) => {
+      $('#accountIDInput').val(accounts[0])
+    })
+  }
+}
+
+$(function () {
+  $(window).on('load', function () {
+    App.init()
+  })
+})
